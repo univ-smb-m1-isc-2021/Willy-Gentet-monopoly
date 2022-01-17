49ef2d9e0f069fe4fe12dc4d4417907065cc4cbd
djavagit <58994972+djavagit@users.noreply.github.com>
2022-01-17 17:30:13 +0100

    fix gare service

--- src/main/java/monopoly/Propriete.java
+++ src/main/java/monopoly/Propriete.java
@@ -1,5 +1,7 @@
 package monopoly;
 
+import java.util.Scanner;
+
 public abstract class Propriete extends Case {
 	
 	private String nom;
@@ -19,6 +21,40 @@ public abstract class Propriete extends Case {
     }
 
     public abstract int sommeAPayer();
+
+
+	public void gereArriveeGareService(Joueur j){
+		if(getProprio() == null) {
+			System.out.println(getNom() + " est libre. Acheter pour " + getPrixAchat() + "M ? (Solde : " + j.getSolde() + "M)");
+			System.out.println("1 = oui; 2 = non");
+			Scanner s = new Scanner(System.in);
+			int reponse = s.nextInt();
+			if(reponse == 1) {
+				if(getPrixAchat() > j.getSolde()) {
+					System.out.println(j.getNom() + " n'as pas assez d'argent (Solde : " + j.getSolde() + "M)");
+				} else {
+					j.debiteCompte(getPrixAchat());
+					j.ajoutePropriete(this);
+					setProprio(j);
+					System.out.println(j.getNom() + " a acheté " + getNom() + ", Solde : " + j.getSolde());
+				}
+			} else {
+				System.out.println(getNom() + " reste libre");
+			}
+		} else {
+			System.out.println(j.getNom() + " arrive sur " + getNom() + ", qui appartient à " + getProprio().getNom());
+			if(j.getNom().equals(getProprio().getNom())) {
+				System.out.println(j.getNom() + " est chez lui");
+			} else {
+				int somme = sommeAPayer();
+				System.out.println(j.getNom() + " doit payer " + somme + "M à " + getProprio().getNom());
+				j.debiteCompte(somme);
+				getProprio().crediteCompte(somme);
+				System.out.println(j.getNom() + " : " + j.getSolde() + "M");
+				System.out.println(getProprio().getNom() + " : " + getProprio().getSolde() + "M");
+			}
+		}
+	}
     
 
     //getter and setters
