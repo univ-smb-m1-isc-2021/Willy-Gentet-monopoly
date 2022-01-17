22d28ef1cc9d13c1a43f615130a681fb353c0696
djavagit <58994972+djavagit@users.noreply.github.com>
2022-01-17 17:21:37 +0100

    optimise etat

--- src/main/java/monopoly/EtatCase.java
+++ src/main/java/monopoly/EtatCase.java
@@ -5,4 +5,16 @@ public interface EtatCase {
     default void messageArrivee(Joueur j, Terrain t){
         System.out.println(j.getNom() + " arrive sur " + t.getNom() + ", qui appartient à " + t.getProprio().getNom());
     }
+    default void gereArriveeNonLibre(Joueur j, Terrain t){
+        if(j.getNom().equals(t.getProprio().getNom())) {
+            System.out.println(j.getNom() + " est chez lui");
+        } else {
+            int somme = t.sommeAPayer();
+            System.out.println(j.getNom() + " doit payer " + somme + "M � " + t.getProprio().getNom());
+            j.debiteCompte(somme);
+            t.getProprio().crediteCompte(somme);
+            System.out.println(j.getNom() + " : " + j.getSolde() + "M");
+            System.out.println(t.getProprio().getNom() + " : " + t.getProprio().getSolde() + "M");
+        }
+    }
 }
--- src/main/java/monopoly/EtatConstruit.java
+++ src/main/java/monopoly/EtatConstruit.java
@@ -9,16 +9,7 @@ public class EtatConstruit implements EtatCase {
 	}
     public void arriveeJoueur(Joueur j) {
 		messageArrivee(j, t);
-		if(j.getNom().equals(t.getProprio().getNom())) {
-    		System.out.println(j.getNom() + " est chez lui");
-    	} else {
-    		int somme = t.sommeAPayer();
-    		System.out.println(j.getNom() + " doit payer " + somme + "M � " + t.getProprio().getNom());
-    		j.debiteCompte(somme);
-    		t.getProprio().crediteCompte(somme);
-    		System.out.println(j.getNom() + " : " + j.getSolde() + "M");
-    		System.out.println(t.getProprio().getNom() + " : " + t.getProprio().getSolde() + "M");
-    	}
+		gereArriveeNonLibre(j, t);
     }
 
 }
