7da459d6577529d055f57c7397175b9fffea900e
djavagit <58994972+djavagit@users.noreply.github.com>
2022-01-17 17:56:15 +0100

    testjoueur

--- src/test/java/JoueurTest.java
+++ src/test/java/JoueurTest.java
@@ -1,4 +1,6 @@
+import monopoly.Gare;
 import monopoly.Joueur;
+import monopoly.Service;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,5 +15,10 @@ public class JoueurTest {
         assertEquals(luc.getSolde(), 1000);
         luc.crediteCompte(200);
         assertEquals(luc.getSolde(), 1200);
+        luc.ajoutePropriete(new Gare("gare 1"));
+        luc.ajoutePropriete(new Gare("gare 2"));
+        assertEquals(luc.nbGares(), 2);
+        luc.ajoutePropriete(new Service("eaux"));
+        assertEquals(luc.nbServices(), 1);
     }
 }
